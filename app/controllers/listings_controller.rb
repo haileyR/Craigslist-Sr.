45c8f4756b5b3423c65987e8ec89646044755205
haileyr & Luke Thomas <pair+haileyr.lukert33@devbootcamp.com>
2015-01-13 14:33:14 -0500

    Build routes and view to create new listing from root.

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,2 +1,23 @@
 class ListingsController < ApplicationController
+  def new
+    listing = Listing.new
+    categories = Category.all
+    render :new, locals: {listing: listing, categories: categories}
+  end
+
+  def create
+    p params[:category]
+    listing = Category.find_by_title(params[:category]).listings.build(listing_params)
+    if listing.save
+      render :show, locals: {listing: listing}
+    else
+      render :new, locals: {listing: listing}
+    end
+  end
+
+  private
+    def listing_params
+
+      params.require(:listing).permit(:title, :price, :location, :content, :owner_email)
+    end
 end
