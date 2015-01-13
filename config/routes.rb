6284efba336207ca29b1013a668c8f0f5dfeda71
haileyr & Luke Thomas <pair+haileyr.lukert33@devbootcamp.com>
2015-01-13 16:09:04 -0500

    Remove unused category routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-  resources :listings, :categories
+  resources :listings
   # You can have the root of your site routed with "root"
   root 'categories#index'
 
+  get '/categories', to: 'categories#index', as: :categories
+  get '/categories/:id', to: 'categories#show', as: :category
   get '/listings/:id/edit/:edit_key', to: 'listings#edit'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
