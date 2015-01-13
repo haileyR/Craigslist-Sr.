7313227350aac087b53d6e1132d6f7ab6dab02da
haileyr & Luke Thomas <pair+haileyr.lukert33@devbootcamp.com>
2015-01-13 10:56:39 -0500

    Add models with a seed file for testing.

---
+++ app/models/listing.rb
@@ -0,0 +1,9 @@
+class Listing < ActiveRecord::Base
+  validates :owner_email, :title, presence: :true
+  validates :edit_key, uniqueness: :true
+  before_create :generate_edit_key
+
+  def generate_edit_key
+    self.edit_key = (0...4).map { (65 + rand(26)).chr }.join(rand(9).to_s)
+  end
+end
