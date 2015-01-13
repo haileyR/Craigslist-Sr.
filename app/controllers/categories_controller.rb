65d16be82b1c0a901f0175b033cca843bd5b0885
haileyr & Luke Thomas <pair+haileyr.lukert33@devbootcamp.com>
2015-01-13 11:54:47 -0500

    Build category#show route with view.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,4 +3,9 @@ class CategoriesController < ApplicationController
     categories = Category.all
     render :index, locals: { categories: categories }
   end
+
+  def show
+    category = Category.find(params[:id])
+    render :show, locals: { category: category, listings: category.listings }
+  end
 end
