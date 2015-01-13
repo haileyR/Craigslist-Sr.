82c8634d7c54ba0b438467d87e6b68b0f46707a4
haileyr & Luke Thomas <pair+haileyr.lukert33@devbootcamp.com>
2015-01-13 15:28:56 -0500

    Edit and update through secrete url with edit_key.

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -19,9 +19,32 @@ class ListingsController < ApplicationController
     end
   end
 
+  def edit
+    listing = Listing.find(params[:id])
+    categories = Category.all
+
+    if params[:edit_key] == listing.edit_key
+      render :edit, locals: {listing: listing, categories: categories}
+    else
+      redirect_to root_path
+    end
+  end
+
+  def update
+    listing = Listing.find(params[:id])
+    listing.update_attributes(listing_params)
+    category = Category.find_by_title(params[:category])
+    listing.category_id = category.id
+    if listing.save
+      render :show, locals: {listing: listing}
+    else
+      render :new, locals: {listing: listing}
+    end
+  end
+
+
   private
     def listing_params
-
       params.require(:listing).permit(:title, :price, :location, :content, :owner_email)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'categories#index'
 
+  get '/listings/:id/edit/:edit_key', to: 'listings#edit'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
